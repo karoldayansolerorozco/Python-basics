382e5665ced76627eef047df6179fad8064b8006
Karol Soler Orozco <148461251+karoldayansolerorozco@users.noreply.github.com>
2023-10-23 11:45:05 -0500

    Add files via upload

---
+++ comparaciones.py
@@ -0,0 +1,15 @@
+
+a = 10
+b = 3
+
+print(f"igual: {a == b}") #compara si las variables son iguales
+print(f"diferente: {a != b}") #compara si las variables son diferentes
+print(f"mayor: {a > b}") #compara si la variable a es mayor que la variable b
+print(f"menor: {a < b}") #compara si la variable a es menor que la variable b
+print(f"mayor o igual: {a >= b}") #compara si la variable a es mayor o igual que la variable b
+print(f"menor o igual: {a <= b}") #compara si la variable a es menor o igual que la variable b
+print(f"and: {a > b and a == b}") #compara si las dos variables son verdaderas
+print(f"or: {a > b or a == b}") #compara si una de las variables es verdadera
+
+
+
